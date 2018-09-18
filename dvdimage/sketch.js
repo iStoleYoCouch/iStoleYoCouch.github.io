19037c11bbfe3fcc93f1c1af863b23fdbfc732be
iStoleYoCouch <43014184+iStoleYoCouch@users.noreply.github.com>
2018-09-18 13:59:16 -0600

    xdab

--- dvdimage/sketch.js
+++ dvdimage/sketch.js
@@ -5,11 +5,40 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let dvd;
+let x, y;
+let dx, dy;
+
+
+function preload(){
+  dvd = loadImage("assets/dvd.jfif");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2 - dvd.width/2;
+  y = height/2 - dvd.height/2;
+  dx = random(3, 8);
+  dy = random(3, 8);
 }
 
 function draw() {
-  background(80, 80, 80)
+  moveDVD();
+  displayDVD();
+}
 
+function moveDVD() {
+  x += dx;
+  y += dy;
+  if (y + dvd.height >= height || y <= 0){
+    dy = dy * -1;
+  }
+  else if (x + dvd.width > width || x <= 0){
+    dx = dx * -1;
+  }
+}
+
+function displayDVD() {
+  background(80, 80, 80)
+  image(dvd, x, y);
 }
